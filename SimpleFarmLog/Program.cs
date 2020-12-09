e69f032124358974e8b996e7e60e1395ce7abe8c
Minh Hien <minhhiena712@gmail.com>
2020-12-09 15:24:06 +0700

    add console app

---
+++ SimpleFarmLog/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleFarmLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
